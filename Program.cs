7427163a24d8e3b6d81a465f86ef787bd6ee52ea
Dhilna M D <144244318+Dhilna31@users.noreply.github.com>
2025-10-14 10:34:28 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,34 @@
-﻿//Solution Average Calculator
+﻿//the while loop
 
-int currentScore;
-int sum = 0;
+Random random = new Random();
+
+int secretNumber = random.Next(1,101);
+//Generates a random number between 1 and 100 (inclusive of 1, exclusive of 101) and
+//stores it in secretNumber.
+int userGuess = 0;
+//the player's guess.
 int counter = 0;
+//how many attempts the user makes.
 
-//do-while loop is a post-test loop
+Console.WriteLine("Guess the number i'm thinking of between 1 and 100");
 
-do
+while (userGuess != secretNumber)
+//Starts a loop that will continue until the user guesses the correct number.
 {
-    Console.WriteLine("Enter your students score.Enter -1 to finish!");
-    currentScore = int.Parse(Console.ReadLine());
-    if (currentScore != -1)
+    counter++;
+    //Increases the attempt counter by one every time the user makes a guess.
+    Console.WriteLine("Enter your Guess:");
+    userGuess = int.Parse(Console.ReadLine());
+    if(userGuess < secretNumber)
     {
-        sum = sum + currentScore;
-        counter++;
+        Console.WriteLine("Too low! Try again.");
+    } else if(userGuess > secretNumber)
+        {
+        Console.WriteLine("Too high! Try again");
     }
-   
+    else
+    {
+        Console.WriteLine("Congratualtions! you guessed the number right! It took you" + counter + "tries!");
+    }
 }
-while (currentScore != -1);
-int average = sum / counter;
-Console.WriteLine("the average is: "+average);
 Console.ReadKey();
