1bb1846538bd2fc4e69083742978a24edf31d646
Dhilna M D <144244318+Dhilna31@users.noreply.github.com>
2025-10-14 10:33:11 +0530

    Solution Average Calculator
    This C# console application calculates the average score of multiple students using a do-while loop. The user is prompted to enter student scores one by one, and the input continues until -1 is entered as a sentinel value to stop. Each valid score is added to a running total, and a counter keeps track of how many scores were entered. Once input ends, the program computes and displays the average. This project demonstrates user input handling, loop control, sentinel-based termination, and basic arithmetic operations in C#.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Solution Average Calculator
+
+int currentScore;
+int sum = 0;
+int counter = 0;
+
+//do-while loop is a post-test loop
+
+do
+{
+    Console.WriteLine("Enter your students score.Enter -1 to finish!");
+    currentScore = int.Parse(Console.ReadLine());
+    if (currentScore != -1)
+    {
+        sum = sum + currentScore;
+        counter++;
+    }
+   
+}
+while (currentScore != -1);
+int average = sum / counter;
+Console.WriteLine("the average is: "+average);
+Console.ReadKey();
