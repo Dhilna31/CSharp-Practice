134d918160f82a817b19c694d379c30344c4e1b7
Dhilna M D <144244318+Dhilna31@users.noreply.github.com>
2025-11-14 12:35:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Channels;
+
+namespace EventsApp
+{
+
+    public delegate void Notify(string message);
+
+    public class EventPublisher
+    {
+        //the 'on' prefix makes it immediately clear that the method
+        //is associated with an event.
+        //it signifies that the method is not just a regular method but
+        //one that is called when a specific event occurs.
+        public event Notify OnNotify;
+
+        public void RaiseEvent(string message)
+        {
+            OnNotify?.Invoke(message); //invoke the event if there are any subscribers
+
+        }
+    }
+
+    public class EventSubscriber
+    {
+        public void OnEventRaised(string message)
+        {
+            Console.WriteLine("Event received: " + message);
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subscriber = new EventSubscriber();
+            publisher.OnNotify += subscriber.OnEventRaised;
+
+            publisher.RaiseEvent("test");
+
+            Console.ReadKey();
+        }
+    }
+}
