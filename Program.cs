d96a2f867d4cea29a902e701a111ddd7e6d8ebcb
Dhilna M D <144244318+Dhilna31@users.noreply.github.com>
2025-11-14 22:49:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+            string text = "hi there,my number is 12334";
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found:\n {1}",matches.Count,text);
+
+            foreach(Match hit in matches)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+
+            }
+
+        }
+    }
+}
